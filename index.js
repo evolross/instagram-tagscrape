9053f452c400f5fc9aebf12f797933c7d923f90f
Ross Newton <rossmnewton@gmail.com>
2020-09-24 16:36:33 -0700

    Updated package to take and use a proxy. Also removed timeout throttling for now. Also uses NODE env var to determine to enforce https. Makes testing over http work with a proxy. Also takes a passed limit now.

--- index.js
+++ index.js
@@ -3,29 +3,31 @@ var request = require('request'),
     listURL = 'https://www.instagram.com/explore/tags/',
     postURL = 'https://www.instagram.com/p/',
     locURL  = 'https://www.instagram.com/explore/locations/',
-    dataExp = /window\._sharedData\s?=\s?({.+);<\/script>/;
+    dataExp = /window\._sharedData\s?=\s?({.+);<\/script>/
 
-exports.promiseTimeout = function(time){
+/*exports.promiseTimeout = function(time){
     return new Promise(function(resolve, reject){
         setTimeout(function(){
             resolve();
         }, time);
     });
-}
+}*/
 
-exports.deepScrapeTagPage = function(tag) {
+exports.deepScrapeTagPage = function(tag, proxy, limit) {
+
     return new Promise(function(resolve, reject) {
         exports.scrapeTagPage(tag).then(function(tagPage) {
 
-            //  Limit tagPage.media to fifteen posts (if no limit it can return up to 70 posts)
-            tagPage.media = tagPage.media.splice(0, 15);
+            //  Limit tagPage.media (if no limit it can return up to 70 posts)
+            if(limit)
+                tagPage.media = tagPage.media.splice(0, limit);
 
             return Promise.map(tagPage.media, function(media, i, len) {
 
                 //  Added a timeout here to try to reduce rate-limiting errors                
-                return exports.promiseTimeout(1500 * (i + 1)).then(function(result) {
+                //return exports.promiseTimeout(1500 * (i + 1)).then(function(result) {
 
-                    return exports.scrapePostPage(media.node.shortcode).then(function(postPage) {
+                    return exports.scrapePostPage(media.node.shortcode, proxy).then(function(postPage) {
 
                         //  Add the result to the media array
                         tagPage.media[i] = postPage;
@@ -42,7 +44,7 @@ exports.deepScrapeTagPage = function(tag) {
                             });
                         }*/
                     })
-                })
+                //})
                 .catch(function(err) {
                     console.log("An error occurred calling scrapePostPage inside deepScrapeTagPage" + ":" + err);
                 });
@@ -58,14 +60,25 @@ exports.deepScrapeTagPage = function(tag) {
     });
 };
 
-exports.scrapeTagPage = function(tag) {
+exports.scrapeTagPage = function(tag, proxy) {
+
     return new Promise(function(resolve, reject){
         if (!tag) return reject(new Error('Argument "tag" must be specified'));
 
-        request(listURL + tag, function(err, response, body){
+        //  Now create a dynmaic requestParams object that dynamically passes if this is production. If not, it allows
+        //  https requests to return unauthorized. And we also dynamically add the proxy if it exists
+        let requestParams = {
+            uri: listURL + tag,
+            rejectUnauthorized: process.env.NODE_ENV === 'production'
+        }
+
+        if(proxy)
+            requestParams.proxy = proxy;
+
+        request(requestParams, function(err, response, body){
             if (err) return reject(err);
 
-            var data = scrape(body)
+            let data = scrape(body)
 
             if (data && data.entry_data && 
                 data.entry_data.TagPage &&
@@ -74,7 +87,7 @@ exports.scrapeTagPage = function(tag) {
                 data.entry_data.TagPage[0].graphql.hashtag &&
                 data.entry_data.TagPage[0].graphql.hashtag.edge_hashtag_to_media &&
                 data.entry_data.TagPage[0].graphql.hashtag.edge_hashtag_to_media.edges) {                
-                var edge_hashtag_to_media = data.entry_data.TagPage[0].graphql.hashtag.edge_hashtag_to_media;
+                let edge_hashtag_to_media = data.entry_data.TagPage[0].graphql.hashtag.edge_hashtag_to_media;
                 resolve({
                     total: edge_hashtag_to_media.count,
                     count: edge_hashtag_to_media.edges.length,
@@ -88,14 +101,24 @@ exports.scrapeTagPage = function(tag) {
     });
 };
 
-exports.scrapePostPage = function(code) {
+exports.scrapePostPage = function(code, proxy) {
     return new Promise(function(resolve, reject){
         if (!code) return reject(new Error('Argument "code" must be specified'));
 
-        request(postURL + code, function(err, response, body){
+        //  Now create a dynmaic requestParams object that dynamically passes if this is production. If not, it allows
+        //  https requests to return unauthorized. And we also dynamically add the proxy if it exists
+        let requestParams = {
+            uri: postURL + code,
+            rejectUnauthorized: process.env.NODE_ENV === 'production'
+        }
+
+        if(proxy)
+            requestParams.proxy = proxy;
+
+        request(requestParams, function(err, response, body){
             if (err) return reject(err);
 
-            var data = scrape(body);
+            let data = scrape(body);
 
             if (data && data.entry_data && 
                 data.entry_data.PostPage &&
@@ -111,14 +134,24 @@ exports.scrapePostPage = function(code) {
     });
 }
 
-exports.scrapeLocationPage = function(id) {
+exports.scrapeLocationPage = function(id, proxy) {
     return new Promise(function(resolve, reject){
         if (!id) return reject(new Error('Argument "id" must be specified'));
+
+        //  Now create a dynmaic requestParams object that dynamically passes if this is production. If not, it allows
+        //  https requests to return unauthorized. And we also dynamically add the proxy if it exists
+        let requestParams = {
+            uri: locURL + id,
+            rejectUnauthorized: process.env.NODE_ENV === 'production'
+        }
+
+        if(proxy)
+            requestParams.proxy = proxy;
         
-        request(locURL + id, function(err, response, body){
+        request(requestParams, function(err, response, body) {
             if (err) return reject(err);
             
-            var data = scrape(body);
+            let data = scrape(body);
 
             if (data && data.entry_data && 
                 data.entry_data.LocationsPage &&
@@ -135,15 +168,17 @@ exports.scrapeLocationPage = function(id) {
 }
 
 var scrape = function(html) {
+
     try {
         var dataString = html.match(dataExp)[1];
         var json = JSON.parse(dataString);
     }
     catch(e) {
-        if (process.env.NODE_ENV != 'production') {
-            console.error('The HTML returned from instagram was not suitable for scraping');
+        if(process.env.NODE_ENV != 'production') {
+            console.error('The HTML returned from Instagram was not suitable for scraping. Or an error occurred and/or request was blocked.');
+            console.log("Error: ", e);
         }
-        return null
+        return null;
     }
 
     return json;
